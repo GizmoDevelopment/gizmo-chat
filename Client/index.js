0fb7199af5481b11519635c87852f7319cd883bd
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-01-25 16:47:59 +0100

    moved client code to gizmo-chat-client

--- Client/index.js
+++
@@ -1,9 +0,0 @@
-const socket = require("socket.io-client").connect("http://localhost:3000", { transports: ["websocket"] });
-
-console.log("Started");
-
-socket.emit("auth", { userKey: "bruh" });
-
-socket.on("response", res => {
-    console.log(res);
-});
